cd0d228ece7df16f44898610bf02c30b2893b9b5
Chaz Larson <chaz@chazlarson.com>
2024-07-15 18:35:33 +0000

    minor tweaks

--- Kometa-walkthrough/newlibs.sh
+++ Kometa-walkthrough/newlibs.sh
@@ -4,7 +4,7 @@
 #  i.e.
 # ./newlibs.sh /opt/kometa-plex/media 2PxWuxX_WHIZBANGt3Z2
 
-rm -fr test_tv_lib/    && ./tv-test-library-builder.sh     && rm -fr "$1/test_tv_lib/"    && rsync -av --progress test_tv_lib "$1/"
-rm -fr test_movie_lib/ && ./walkthrough-library-builder.sh && rm -fr "$1/test_movie_lib/" && rsync -av --progress test_movie_lib "$1/"
+rm -fr test_tv_lib/    && ./tv-library-builder.sh     && rm -fr "$1/test_tv_lib/"    && rsync -av --progress test_tv_lib "$1/"
+rm -fr test_movie_lib/ && ./movie-library-builder.sh && rm -fr "$1/test_movie_lib/" && rsync -av --progress test_movie_lib "$1/"
 
 curl -fLv http://192.168.1.11:32400/library/sections/all/refresh?X-Plex-Token=$2
